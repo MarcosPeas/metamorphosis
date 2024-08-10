a5d549d21963f5405d5d5ada7f0b2a0b521ccc1d
Marcos Costa <marcos.costa@granter.com.br>
2024-08-10 12:39:55 -0300

    adicionando tipos de dados dos objetos de valor

---
+++ lib/src/domain/_core/constants/types.dart
@@ -0,0 +1,12 @@
+class Types {
+  static List<String> types = [
+    'String',
+    'Integer',
+    'Long',
+    'Double',
+    'Boolean',
+    'Date',
+    'Time',
+    'DateTime',
+  ];
+}
--- lib/src/presenter/home/entity/entity_page.dart
+++ lib/src/presenter/home/entity/entity_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:metamorphis/src/domain/_core/constants/types.dart';
 import 'package:metamorphis/src/domain/value_object/entities/value_object.dart';
 import 'package:metamorphis/src/presenter/_core/view_models/entity_view_model.dart';
 import 'package:metamorphis/src/presenter/home/entity/entity_controller.dart';
@@ -58,6 +59,16 @@ class _EntityPageState extends State<EntityPage> {
           child: ListenableBuilder(
             listenable: entityStore,
             builder: (context, _) {
+              if (entityStore.loading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (entityStore.valueObjects.isEmpty) {
+                return const Center(
+                  child: Text('No value objects'),
+                );
+              }
               final valueObjects = entityStore.valueObjects;
               return ListView.builder(
                 shrinkWrap: true,
@@ -104,28 +115,68 @@ class _EntityPageState extends State<EntityPage> {
 
   void _showDialogCreateValueObject() {
     final nameController = TextEditingController();
+    final selectedType = ValueNotifier('String');
+    final isNullable = ValueNotifier(false);
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text('Create a value object'),
-          content: TextField(
-            controller: nameController,
-            onSubmitted: (text) {
-              if (text.isEmpty) {
-                return;
-              }
-              controller.createValueObject(
-                entity: widget.entity,
-                name: nameController.text.trim(),
-                type: 'String',
-                nullable: false,
-              );
-              context.pop();
-            },
-            decoration: const InputDecoration(
-              labelText: 'Name',
-            ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: nameController,
+                onSubmitted: (text) {
+                  if (text.isEmpty) {
+                    return;
+                  }
+                  controller.createValueObject(
+                    entity: widget.entity,
+                    name: nameController.text.trim(),
+                    type: selectedType.value,
+                    nullable: isNullable.value,
+                  );
+                  context.pop();
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text('Data type'),
+              ValueListenableBuilder(
+                valueListenable: selectedType,
+                builder: (_, type, __) {
+                  return DropdownButton(
+                    value: type,
+                    onChanged: (value) {
+                      selectedType.value = value.toString();
+                    },
+                    items: Types.types.map((type) {
+                      return DropdownMenuItem(
+                        value: type,
+                        child: Text(type),
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
+              const SizedBox(height: 16),
+              ValueListenableBuilder(
+                valueListenable: isNullable,
+                builder: (_, value, __) {
+                  return CheckboxListTile(
+                    title: const Text('Nullable'),
+                    value: value,
+                    onChanged: (changedValue) {
+                      isNullable.value = changedValue ?? false;
+                    },
+                  );
+                },
+              ),
+            ],
           ),
           actions: [
             TextButton(
@@ -139,8 +190,8 @@ class _EntityPageState extends State<EntityPage> {
                 controller.createValueObject(
                   entity: widget.entity,
                   name: nameController.text.trim(),
-                  type: 'String',
-                  nullable: false,
+                  type: selectedType.value,
+                  nullable: isNullable.value,
                 );
                 context.pop();
               },
@@ -154,30 +205,70 @@ class _EntityPageState extends State<EntityPage> {
 
   void _showDialogUpdateValueObject(int viewIndex, ValueObject valueObject) {
     final nameController = TextEditingController();
+    final selectedType = ValueNotifier(valueObject.type);
+    final isNullable = ValueNotifier(valueObject.nullable);
     nameController.text = valueObject.name;
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text('Update a value object'),
-          content: TextField(
-            controller: nameController,
-            onSubmitted: (text) {
-              if (text.isEmpty) {
-                return;
-              }
-              controller.updateValueObject(
-                entity: widget.entity,
-                name: nameController.text.trim(),
-                type: 'String',
-                nullable: false,
-                viewIndex: viewIndex,
-              );
-              context.pop();
-            },
-            decoration: const InputDecoration(
-              labelText: 'Name',
-            ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: nameController,
+                onSubmitted: (text) {
+                  if (text.isEmpty) {
+                    return;
+                  }
+                  controller.updateValueObject(
+                    entity: widget.entity,
+                    name: nameController.text.trim(),
+                    type: selectedType.value,
+                    nullable: isNullable.value,
+                    viewIndex: viewIndex,
+                  );
+                  context.pop();
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text('Data type'),
+              ValueListenableBuilder(
+                valueListenable: selectedType,
+                builder: (_, type, __) {
+                  return DropdownButton(
+                    value: type,
+                    onChanged: (value) {
+                      selectedType.value = value.toString();
+                    },
+                    items: Types.types.map((type) {
+                      return DropdownMenuItem(
+                        value: type,
+                        child: Text(type),
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
+              const SizedBox(height: 16),
+              ValueListenableBuilder(
+                valueListenable: isNullable,
+                builder: (_, value, __) {
+                  return CheckboxListTile(
+                    title: const Text('Nullable'),
+                    value: value,
+                    onChanged: (changedValue) {
+                      isNullable.value = changedValue ?? false;
+                    },
+                  );
+                },
+              ),
+            ],
           ),
           actions: [
             TextButton(
@@ -191,8 +282,8 @@ class _EntityPageState extends State<EntityPage> {
                 controller.updateValueObject(
                   entity: widget.entity,
                   name: nameController.text.trim(),
-                  type: 'String',
-                  nullable: false,
+                  type: selectedType.value,
+                  nullable: isNullable.value,
                   viewIndex: viewIndex,
                 );
                 context.pop();
